78d87a3877493a79f83d84910caf345c7b36740f
inadram <inadram@gmail.com>
2015-04-08 23:02:04 +0100

    replace all spcaes in place with %20

--- src/main/dataStructures/arraysAndStrings/Four/StringSpace.java
+++ src/main/dataStructures/arraysAndStrings/Four/StringSpace.java
@@ -18,6 +18,23 @@ public class StringSpace {
         return characters;
     }
 
+    public char[] replaceWith20PercentInPlace(char[] someString, int length) {
+        int numberOfSpaces = countNumberOfSpaces(someString);
+        int indexLengthOfNewString = numberOfSpaces * 2 + length;
+        for(int i=length-1;i>=0;i--){
+            if(someString[i]==' '){
+                someString[indexLengthOfNewString-1]='0';
+                someString[indexLengthOfNewString-2]='2';
+                someString[indexLengthOfNewString-3]='%';
+                indexLengthOfNewString-=3;
+            }else{
+                someString[indexLengthOfNewString -1]=someString[i];
+                indexLengthOfNewString-=1;
+            }
+        }
+        return someString;
+    }
+
     private int countNumberOfSpaces(char[] someString) {
         int counter=0;
         for(char character:someString){
--- src/test/StringSpaceTest.java
+++ src/test/StringSpaceTest.java
@@ -15,4 +15,20 @@ public class StringSpaceTest {
 
         assertEquals(new String(actualString),new String(expectedString));
     }
+
+    @Test
+    public void test_all_spaces_replace_with_20PercentInPlace(){
+        StringSpace stringSpace=new StringSpace();
+        char[] someCharArray = new char["some%20string%20to%20re%20place%20".length()];
+        char[] original = "some string to re place ".toCharArray();
+        int index=0;
+        for(char c :original){
+            someCharArray[index]=c;
+            index++;
+        }
+        char[] actualString= stringSpace.replaceWith20PercentInPlace(someCharArray,original.length);
+        char[] expectedString= "some%20string%20to%20re%20place%20".toCharArray();
+
+        assertEquals(new String(actualString),new String(expectedString));
+    }
 }
