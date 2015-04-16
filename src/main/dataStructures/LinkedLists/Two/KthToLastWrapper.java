3a1fadb17e4ef46416b6e0f9cb36ac213ce517e7
inadram <inadram@gmail.com>
2015-04-16 19:21:13 +0100

    use wrapper to find kth element to last

---
+++ src/main/dataStructures/LinkedLists/Two/KthToLastWrapper.java
@@ -0,0 +1,26 @@
+package main.dataStructures.LinkedLists.Two;
+
+import main.dataStructures.LinkedLists.Lib.LinkedListNode;
+import main.dataStructures.LinkedLists.Two.Lib.IntWrapper;
+
+public class KthToLastWrapper {
+    public int get(LinkedListNode linkedListNode, int kthToLast) {
+        LinkedListNode node =getKthVNode(linkedListNode, kthToLast, new IntWrapper());
+        return (node!=null)?node.data:-1;
+    }
+
+    private LinkedListNode getKthVNode(LinkedListNode linkedListNode, int kthToLast, IntWrapper intWrapper) {
+        if(linkedListNode==null){
+            return null;
+        }
+
+        LinkedListNode node = getKthVNode(linkedListNode.next, kthToLast, intWrapper);
+        intWrapper.setId(intWrapper.getId()+1);
+        if(intWrapper.getId()==kthToLast){
+           return linkedListNode;
+        }else{
+            return node;
+        }
+    }
+
+}
