9f568377e0c024cd53f1e6790cf26d0703268332
inadram <inadram@gmail.com>
2015-04-22 22:50:15 +0100

    check half of the linkedlist

--- src/main/dataStructures/LinkedLists/Lib/NodeEquality.java
+++ src/main/dataStructures/LinkedLists/Lib/NodeEquality.java
@@ -12,4 +12,16 @@ public class NodeEquality {
         }
         return true;
     }
+
+    public static boolean check(LinkedListNode first, LinkedListNode second, int size) {
+        while (first!=null && size!=0){
+            if(first.data!=second.data){
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+            size--;
+        }
+        return true;
+    }
 }
