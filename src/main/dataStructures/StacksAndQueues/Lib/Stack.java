85f80e91b726915f70283be9557b407d4e14c8dd
inadram <inadram@gmail.com>
2015-04-26 00:48:19 +0100

    create stack and queue library classes  with linkedListNode

---
+++ src/main/dataStructures/StacksAndQueues/Lib/Stack.java
@@ -0,0 +1,29 @@
+package main.dataStructures.StacksAndQueues.Lib;
+
+import main.dataStructures.LinkedLists.Lib.LinkedListNode;
+
+public class Stack {
+    LinkedListNode head = null;
+    LinkedListNode tail = null;
+
+    public void push(int i) {
+        head = new LinkedListNode(i);
+        head.next = tail;
+        tail = head;
+
+    }
+
+    public int peek() {
+        return head.data;
+    }
+
+    public int pop() {
+        if (head != null) {
+            int data = head.data;
+            head = head.next;
+            return data;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ src/test/StackTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import junit.framework.TestCase;
+import main.dataStructures.StacksAndQueues.Lib.Stack;
+import org.junit.Test;
+
+public class StackTest extends TestCase {
+
+    @Test
+    public void test_push_to_stack_add_item_to_head(){
+       Stack stack =new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertEquals(3, stack.peek());
+    }
+
+    @Test
+    public void test_pop_from_stack_return_item_from_head(){
+        Stack queue =new Stack();
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+
+        assertEquals(3, queue.pop());
+        assertEquals(2, queue.pop());
+    }
+
+}
