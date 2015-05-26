099784d1db6e637b362fc7a3552b8b8f88ec65c8
inadram <inadram@gmail.com>
2015-05-26 17:42:41 +0100

    revert back rollback

---
+++ src/test/StaircaseTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import junit.framework.TestCase;
+import main.ConceptsAndAlgorothms.RecursionAndDynamicProgramming.One.Staircase;
+import org.testng.annotations.Test;
+
+public class StaircaseTest extends TestCase {
+
+    @Test
+    public void staircase_with_1_step() {
+
+        Staircase staircase = new Staircase();
+        assertEquals(1, staircase.navigate(1));
+    }
+
+    @Test
+    public void staircase_with_2_step() {
+
+        Staircase staircase = new Staircase();
+        assertEquals(2, staircase.navigate(2));
+    }
+
+    @Test
+    public void staircase_with_3_step() {
+
+        Staircase staircase = new Staircase();
+        assertEquals(4, staircase.navigate(3));
+    }
+
+    @Test
+    public void staircase_with_4_step() {
+
+        Staircase staircase = new Staircase();
+        assertEquals(7, staircase.navigate(4));
+    }
+
+    @Test
+    public void staircase_with_5_step() {
+
+        Staircase staircase = new Staircase();
+        assertEquals(13, staircase.navigate(5));
+    }
+}
