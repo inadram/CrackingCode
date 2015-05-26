09ac0b889ee03e26829e1caeb495ff210abe70bb
inadram <inadram@gmail.com>
2015-05-26 19:41:33 +0100

    calculate path in dynamic way and arraylist path

---
+++ src/test/XYGridPathTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import junit.framework.TestCase;
+import main.ConceptsAndAlgorothms.RecursionAndDynamicProgramming.Two.XYGridPath;
+import org.junit.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class XYGridPathTest extends TestCase {
+
+    XYGridPath grid = new XYGridPath();
+
+    @Test
+    public void test_grid_with_two_cell() {
+        assertEquals(2, grid.navigate(1, 1,new ArrayList<Point>()));
+    }
+
+    @Test
+    public void test_grid_with_three_cell() {
+        assertEquals(6, grid.navigate(2, 2,new ArrayList<Point>()));
+    }
+
+    @Test
+    public void test_grid_with_four_cell() {
+        assertEquals(20, grid.navigate(3, 3,new ArrayList<Point>()));
+    }
+
+}
