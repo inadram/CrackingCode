291a80a8d5074bac36ab5ae1b79b19a0eee7bd49
inadram <inadram@gmail.com>
2015-05-29 09:18:25 +0100

    reformat

--- src/test/MagicIndexNoneDistinctTest.java
+++ src/test/MagicIndexNoneDistinctTest.java
@@ -8,7 +8,6 @@ import static junit.framework.TestCase.assertEquals;
 public class MagicIndexNoneDistinctTest  {
     @Test
     public void find_expected_magic_index(){
-
         int[] sortedArray = new int[]{1,2,2,2,50,60,70};
         MagicIndexNoneDistinct magicIndex = new MagicIndexNoneDistinct();
         assertEquals(2, magicIndex.find(sortedArray,0,sortedArray.length-1));
