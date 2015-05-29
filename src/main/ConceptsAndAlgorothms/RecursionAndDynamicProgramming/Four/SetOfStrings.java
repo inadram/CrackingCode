0da5e5b8069a0cdcbdd5853f7295017c3c4188c5
inadram <inadram@gmail.com>
2015-05-29 15:26:33 +0100

    find all subsets recursively and iteratively

---
+++ src/main/ConceptsAndAlgorothms/RecursionAndDynamicProgramming/Four/SetOfStrings.java
@@ -0,0 +1,30 @@
+package main.ConceptsAndAlgorothms.RecursionAndDynamicProgramming.Four;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOfStrings {
+    public ArrayList<Set<String>> getSubsets(ArrayList<Set<String>> setOfStrings, int index, ArrayList<Set<String>> subsets) {
+        if(index>=setOfStrings.size()){
+            subsets.add(new HashSet<String>());
+            return subsets;
+        }
+        Set<String> set = setOfStrings.get(index);
+        subsets =addSubset(set,0, subsets);
+        return getSubsets(setOfStrings,++index,subsets);
+    }
+
+    public  ArrayList<Set<String>> addSubset(Set<String> set, int index, ArrayList<Set<String>> subsets){
+        if(index>=subsets.size()){
+            subsets.add(set);
+            return subsets;
+        }
+        Set<String>  subset= new HashSet<String>(subsets.get(index)) ;
+        subset.addAll(set);
+        if(!subsets.contains(subset)){
+            subsets.add(subset);
+        }
+        return addSubset(set, ++index,subsets);
+    }
+}
