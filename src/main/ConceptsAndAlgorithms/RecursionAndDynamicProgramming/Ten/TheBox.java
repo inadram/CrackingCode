3293c43e78656f40a73943681690ec9a92d95e18
inadram <inadram@gmail.com>
2015-06-26 00:11:11 +0100

    find maximum height of stacks

---
+++ src/main/ConceptsAndAlgorithms/RecursionAndDynamicProgramming/Ten/TheBox.java
@@ -0,0 +1,12 @@
+package main.ConceptsAndAlgorithms.RecursionAndDynamicProgramming.Ten;
+
+public class TheBox {
+    public int height;
+    public int width;
+    public int depth;
+    public TheBox(int height,int width,int depth){
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+}
