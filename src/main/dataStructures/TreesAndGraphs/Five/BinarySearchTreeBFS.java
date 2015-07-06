b0cc728ab51654f4e7ac1dae0a05c91ce37f1b10
inadram <inadram@gmail.com>
2015-07-06 09:11:32 +0100

    check if a binary tree is binary search tree with in order dfs and bfs

---
+++ src/main/dataStructures/TreesAndGraphs/Five/BinarySearchTreeBFS.java
@@ -0,0 +1,44 @@
+package main.dataStructures.TreesAndGraphs.Five;
+
+import main.dataStructures.TreesAndGraphs.Lib.Tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+            100
+         50     150
+       40  65
+             70
+               102
+    */
+public class BinarySearchTreeBFS {
+    public boolean check(TreeNode treeNode) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(treeNode);
+        boolean isBinarySearchTree = true;
+
+        while (queue.size()!=0 && isBinarySearchTree){
+            TreeNode root= queue.remove();
+            isBinarySearchTree = BFS(root.data,root.left,false) && BFS(root.data,root.right,true);
+            if(root.left!=null)queue.add(root.left);
+            if(root.right!=null)queue.add(root.right);
+        }
+        return isBinarySearchTree;
+    }
+
+    private boolean BFS(int data,TreeNode node, boolean shouldBeSmaller) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        boolean isBinarySearchTree = true;
+        if(node!=null) {
+            queue.add(node);
+        }
+        while (queue.size()!=0 && isBinarySearchTree){
+           TreeNode root= queue.remove();
+           isBinarySearchTree=(data<root.data ==shouldBeSmaller);
+           if(root.left!=null)queue.add(root.left);
+           if(root.right!=null)queue.add(root.right);
+        }
+        return isBinarySearchTree;
+    }
+}
---
+++ src/test/BinarySearchTreeDFSArrayTest.java
@@ -0,0 +1,67 @@
+package test;
+
+import main.dataStructures.TreesAndGraphs.Five.BinarySearchTreeDFSArray;
+import main.dataStructures.TreesAndGraphs.Lib.Tree.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BinarySearchTreeDFSArrayTest {
+
+     @Test
+    public void check_if_a_binary_search_tree_valid(){
+
+         /*
+                100
+             50     150
+           40  65
+                 70
+
+        */
+
+
+         BinarySearchTreeDFSArray binarySearchTree=new BinarySearchTreeDFSArray();
+         TreeNode treeNode = new TreeNode(100);
+         TreeNode node_50 = new TreeNode(50);
+         TreeNode node_40 = new TreeNode(40);
+         TreeNode node_65 = new TreeNode(65);
+         TreeNode node_70 = new TreeNode(70);
+         TreeNode node_150 = new TreeNode(150);
+         treeNode.left = node_50;
+         treeNode.right = node_150;
+         node_50.left = node_40;
+         node_50.right = node_65;
+         node_65.right = node_70;
+         assertTrue(binarySearchTree.check(treeNode));
+    }
+
+    @Test
+    public void check_if_a_not_binary_search_tree_valid(){
+
+         /*
+                100
+             50     150
+           40  65
+                 70
+                   102
+        */
+
+
+        BinarySearchTreeDFSArray binarySearchTree=new BinarySearchTreeDFSArray();
+        TreeNode treeNode = new TreeNode(100);
+        TreeNode node_50 = new TreeNode(50);
+        TreeNode node_40 = new TreeNode(40);
+        TreeNode node_65 = new TreeNode(65);
+        TreeNode node_70 = new TreeNode(70);
+        TreeNode node_102 = new TreeNode(102);
+        TreeNode node_150 = new TreeNode(150);
+        treeNode.left = node_50;
+        treeNode.right = node_150;
+        node_50.left = node_40;
+        node_50.right = node_65;
+        node_65.right = node_70;
+        node_70.left= node_102;
+        assertFalse(binarySearchTree.check(treeNode));
+    }
+}
