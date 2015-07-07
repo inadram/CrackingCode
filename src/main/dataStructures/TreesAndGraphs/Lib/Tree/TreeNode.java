5fdeed49d04b6e2bbd2a0757cd1d936e27deae38
inadram <inadram@gmail.com>
2015-07-07 17:03:15 +0100

    find next node in TreeNode with in order travers

--- src/main/dataStructures/TreesAndGraphs/Lib/Tree/TreeNode.java
+++ src/main/dataStructures/TreesAndGraphs/Lib/Tree/TreeNode.java
@@ -3,6 +3,7 @@ package main.dataStructures.TreesAndGraphs.Lib.Tree;
 public class TreeNode {
     public TreeNode left;
     public TreeNode right;
+    public TreeNode parent;
     public int data;
 
     public TreeNode(int data){
