69ba6f127c664b865c2d88a781d05e42fbbaf7bf
inadram <inadram@gmail.com>
2015-07-07 07:36:33 +0100

    check binary tree nodes values are between min and max bounds

---
+++ src/main/dataStructures/TreesAndGraphs/Five/BinarySearchTreeMinMaxDFS.java
@@ -0,0 +1,31 @@
+package main.dataStructures.TreesAndGraphs.Five;
+
+import main.dataStructures.TreesAndGraphs.Lib.Tree.TreeNode;
+
+/*
+            100
+         50     150
+       40  65
+             70
+               102
+    */
+
+
+public class BinarySearchTreeMinMaxDFS {
+    public boolean check(TreeNode treeNode) {
+        return minMaxDFS(treeNode,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+
+    private boolean minMaxDFS(TreeNode treeNode, int minValue, int maxValue) {
+        if (treeNode == null) {
+            return true;
+        }
+        if (treeNode.data < minValue || treeNode.data > maxValue) {
+            return false;
+        }
+
+        return minMaxDFS(treeNode.left, minValue, treeNode.data) && minMaxDFS(treeNode.right, treeNode.data, maxValue);
+    }
+
+
+}
---
+++ src/test/BinarySearchTreeMinMaxDFSTest.java
@@ -0,0 +1,101 @@
+package test;
+
+import main.dataStructures.TreesAndGraphs.Five.BinarySearchTreeDFS;
+import main.dataStructures.TreesAndGraphs.Five.BinarySearchTreeMinMaxDFS;
+import main.dataStructures.TreesAndGraphs.Lib.Tree.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BinarySearchTreeMinMaxDFSTest {
+
+     @Test
+    public void check_if_a_binary_search_tree_valid(){
+
+         /*
+                100
+             50     150
+           40  65
+                 70
+
+        */
+
+
+         BinarySearchTreeMinMaxDFS binarySearchTree=new BinarySearchTreeMinMaxDFS();
+         TreeNode treeNode = new TreeNode(100);
+         TreeNode node_50 = new TreeNode(50);
+         TreeNode node_40 = new TreeNode(40);
+         TreeNode node_65 = new TreeNode(65);
+         TreeNode node_70 = new TreeNode(70);
+         TreeNode node_150 = new TreeNode(150);
+         treeNode.left = node_50;
+         treeNode.right = node_150;
+         node_50.left = node_40;
+         node_50.right = node_65;
+         node_65.right = node_70;
+         assertTrue(binarySearchTree.check(treeNode));
+    }
+
+    @Test
+    public void check_if_a_not_binary_search_tree_valid(){
+
+         /*
+                100
+             50     150
+           40  65
+                 70
+                   102
+        */
+
+
+        BinarySearchTreeMinMaxDFS binarySearchTree=new BinarySearchTreeMinMaxDFS();
+        TreeNode treeNode = new TreeNode(100);
+        TreeNode node_50 = new TreeNode(50);
+        TreeNode node_40 = new TreeNode(40);
+        TreeNode node_65 = new TreeNode(65);
+        TreeNode node_70 = new TreeNode(70);
+        TreeNode node_102 = new TreeNode(102);
+        TreeNode node_150 = new TreeNode(150);
+        treeNode.left = node_50;
+        treeNode.right = node_150;
+        node_50.left = node_40;
+        node_50.right = node_65;
+        node_65.right = node_70;
+        node_70.right= node_102;
+        assertFalse(binarySearchTree.check(treeNode));
+    }
+
+    @Test
+    public void check_if_a_not_binary_search_tree_valid_with_desendent(){
+
+         /*
+                100
+             50     150
+           40  65
+                 70
+                   99
+                63
+
+        */
+
+
+        BinarySearchTreeDFS binarySearchTree=new BinarySearchTreeDFS();
+        TreeNode treeNode = new TreeNode(100);
+        TreeNode node_50 = new TreeNode(50);
+        TreeNode node_40 = new TreeNode(40);
+        TreeNode node_65 = new TreeNode(65);
+        TreeNode node_70 = new TreeNode(70);
+        TreeNode node_99 = new TreeNode(99);
+        TreeNode node_98 = new TreeNode(98);
+        TreeNode node_150 = new TreeNode(150);
+        treeNode.left = node_50;
+        treeNode.right = node_150;
+        node_50.left = node_40;
+        node_50.right = node_65;
+        node_65.right = node_70;
+        node_70.right= node_99;
+        node_150.left = node_98;
+        assertFalse(binarySearchTree.check(treeNode));
+    }
+}
