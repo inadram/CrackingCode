6f25e727cda07b3fa5a496fc9ca9d65951fc78b3
inadram <inadram@gmail.com>
2015-07-21 18:40:26 +0100

    normalise relative path

---
+++ src/main/Questions/ID5665972218232832/RelativePath.java
@@ -0,0 +1,22 @@
+package main.Questions.ID5665972218232832;
+
+public class RelativePath {
+    public String normalise(String path) {
+        path = removeCurrentDirectorySign(path);
+        return removeParentDirectorySign(path);
+    }
+
+    private String removeParentDirectorySign(String path) {
+        String[] pathArray =path.split("\\\\.\\.");
+        StringBuilder pathBuilder = new StringBuilder();
+        for (String aPathArray : pathArray) {
+            if (pathBuilder.length()>0) pathBuilder.delete(pathBuilder.lastIndexOf("\\"), pathBuilder.length());
+            pathBuilder.append(aPathArray);
+        }
+        return pathBuilder.toString();
+    }
+
+    private String removeCurrentDirectorySign(String path) {
+        return path.replace("\\.\\", "\\");
+    }
+}
---
+++ src/main/Questions/README.md
@@ -0,0 +1,4 @@
+1.  Given a string representing relative path write a function which normalizes this path (i.e. replaces ".."). 
+    Example: 
+    input: \a\b\..\foo.txt 
+    output: \a\foo.txt
