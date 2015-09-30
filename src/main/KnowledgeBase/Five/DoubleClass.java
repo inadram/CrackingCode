cc78f8009131a625e18c87399b1d1d7f21c46804
inadram <inadram@gmail.com>
2015-09-30 22:42:25 +0100

    use reflection to invoke a method

---
+++ src/main/KnowledgeBase/Five/DoubleClass.java
@@ -0,0 +1,15 @@
+package main.KnowledgeBase.Five;
+
+public class DoubleClass {
+    private final double x;
+    private final double y;
+
+    public DoubleClass(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double sum(){
+        return x+y;
+    }
+}
