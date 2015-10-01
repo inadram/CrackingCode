0ff00ebe6aa871f55d7bc230b303194c49330b44
inadram <inadram@gmail.com>
2015-10-01 09:46:42 +0100

    create circular array test case

---
+++ src/main/KnowledgeBase/Six/CircularArray.java
@@ -0,0 +1,16 @@
+package main.KnowledgeBase.Six;
+
+public class CircularArray {
+    public CircularArray(int Length) {
+
+    }
+
+    public void set(int index, String value) {
+
+
+    }
+
+    public String get(int index) {
+        return null;
+    }
+}
---
+++ src/test/CircularArrayTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import main.KnowledgeBase.Six.CircularArray;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CircularArrayTest {
+
+    @Test
+    public void test_circular_array(){
+        CircularArray circularArray = new CircularArray(5);
+        circularArray.set(0,"a");
+        circularArray.set(1,"b");
+        circularArray.set(2,"c");
+        circularArray.set(3,"d");
+        circularArray.set(4,"e");
+        circularArray.set(5,"f");
+        circularArray.set(6,"g");
+        assertEquals("f",circularArray.get(0));
+    }
+
+}
